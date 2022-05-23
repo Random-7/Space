0d514eeb29f75cc5dcf43cec780e7c2f4649e844
Jon Fingas <jon.fingas@gmail.com>
2022-05-23 18:28:57 -0500

    cleanup in player class

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -12,15 +12,4 @@ public class Player : MonoBehaviour
     public float GetFire1Rate() { return fire1Rate; }
     public float GetFire2Rate() { return fire2Rate; }
     // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-       
-    }
-
 }
