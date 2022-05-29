8beffadfa5f2afc4978aeda781a59f4cef62088b
Jon Fingas <jon.fingas@gmail.com>
2022-05-29 17:40:04 -0500

    Creation of basic function menu

---
+++ Assets/Scripts/UI/MenuNav.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuNav : MonoBehaviour
+{
+    public void PlayClicked()
+    {
+        print("Play clicked");
+
+    }
+    public void CreditsClicked()
+    {
+        print("Credits clicked");
+    }
+    public void QuitClicked()
+    {
+        print("Quit clicked");
+    }
+}
